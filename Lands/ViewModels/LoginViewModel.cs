cbf42f736d8360eaaeba7b6f014c04a836ddc6f0
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-06-05 13:33:18 +0200

    Añadido ViewModel para Login y Bindeo. Además de sus atributos, comandos y constructor.

---
+++ Lands/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace Lands.ViewModels
+{
+    public class LoginViewModel
+    {
+        #region Properties
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsRunning { get; set; }
+        public bool IsRemembered { get; set; }
+        #endregion
+
+        #region Constructors
+        public LoginViewModel()
+        {
+            this.IsRemembered = true;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand Login { get; set; }
+        public ICommand Register { get; set; }
+        #endregion
+    }
+}
