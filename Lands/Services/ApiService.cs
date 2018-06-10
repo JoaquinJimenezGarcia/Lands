fc43923a9031d9b15edd1cd94677b4d677d2d259
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-06-10 20:15:32 +0200

    Creados modelo de Response y servicio de  Api

---
+++ Lands/Services/ApiService.cs
@@ -0,0 +1,44 @@
+﻿namespace Lands.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Models;
+    using Newtonsoft.Json;
+    using Plugin.Connectivity;
+
+    public class ApiService
+    {
+        public async Task<Response> CheckConnection()
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = "Please, turn on your internet connection."
+                };
+            }
+
+            var isReachable = await CrossConnectivity.Current.IsRemoteReachable("goole.com");
+
+            if (!isReachable) 
+            {
+                return new Response 
+                {
+                    IsSuccess = false,
+                    Message = "The network is no reachable."
+                }
+            }
+
+            return new Response
+            {
+                IsSuccess = true,
+                Message = "Connected!"
+            }
+        }
+    }
+}
