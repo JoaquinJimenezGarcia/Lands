010296236412034c0a5841cd134b001284840038
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-06-10 19:44:37 +0200

    Agregados modelos para usar en el parseo del JSON

---
+++ Lands/Models/Language.cs
@@ -0,0 +1,19 @@
+﻿namespace Lands.Models
+{
+    using Newtonsoft.Json;
+
+    public class Language
+    {
+        [JsonProperty(PropertyName = "iso639_1")]
+        public string ISO6391 { get; set; }
+
+        [JsonProperty(PropertyName = "iso639_2")]
+        public string ISO6392 { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "nativeName")]
+        public string NativeName { get; set; }
+    }
+}
