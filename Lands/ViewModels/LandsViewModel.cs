db24cf5c954f70c3e9e890acc9401b9f0f467f6c
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-07-29 20:05:06 +0200

    Added Search bar to filter

--- Lands/ViewModels/LandsViewModel.cs
+++ Lands/ViewModels/LandsViewModel.cs
@@ -6,6 +6,9 @@
     using Xamarin.Forms;
     using Models;
     using Services;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
+    using System.Linq;
 
     public class LandsViewModel : BaseViewModel
     {
@@ -15,6 +18,9 @@
 
         #region Attributes
         private ObservableCollection<Land> lands;
+        private bool isRefreshing;
+        private string filter;
+        private List<Land> landsList;
         #endregion
 
         #region Properties
@@ -22,6 +28,22 @@
         {
             get { return this.lands; }
             set { SetValue(ref this.lands, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+
+        public string Filter
+        {
+            get { return this.filter; }
+            set 
+            { 
+                SetValue(ref this.filter, value);
+                this.Search();
+            }
         }
         #endregion
 
@@ -36,10 +58,12 @@
         #region Methods
         private async void LoadLands()
         {
+            this.IsRefreshing = true;
             var connection = await this.apiService.CheckConnection();
 
-            if(!connection.IsSuccess)
+            if (!connection.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error with the connection.",
                     connection.Message,
@@ -52,8 +76,9 @@
 
             var response = await this.apiService.GetList<Land>("http://restcountries.eu", "/rest", "/v2/all");
 
-            if(!response.IsSuccess)
+            if (!response.IsSuccess)
             {
+                this.IsRefreshing = false;
                 await Application.Current.MainPage.DisplayAlert(
                     "Error",
                     response.Message,
@@ -64,8 +89,42 @@
                 return;
             }
 
-            var list = (List<Land>)response.Result;
-            this.Lands = new ObservableCollection<Land>(list);
+            this.landsList = (List<Land>)response.Result;
+            this.Lands = new ObservableCollection<Land>(this.landsList);
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand 
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+
+        public ICommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(Search);
+            } 
+        }
+
+        private void Search()
+        {
+            if (string.IsNullOrEmpty(this.Filter))
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList);
+            } 
+            else 
+            {
+                this.Lands = new ObservableCollection<Land>(
+                    this.landsList.Where(l => l.Name.ToLower().Contains(this.Filter.ToLower()) || 
+                                         l.Capital.ToLower().Contains(this.Filter.ToLower())));
+            }
         }
         #endregion
     }
