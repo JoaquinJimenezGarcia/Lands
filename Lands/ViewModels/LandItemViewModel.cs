1300437c73ec5a780ad19abd6f612e8d4625f192
Joaquín Jiménez García <garcia.jjimenez@gmail.com>
2018-08-05 19:55:01 +0200

    Al seleccionar un país, te lleva a una vista con su nombre.

---
+++ Lands/ViewModels/LandItemViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Lands.Models;
+using Xamarin.Forms;
+using Lands.Views;
+
+namespace Lands.ViewModels
+{
+    public class LandItemViewModel : Land
+    {
+        #region Commands
+        public ICommand SelectLandCommand
+        {
+            get { return new RelayCommand(SelectLand); }
+        }
+
+        private async void SelectLand()
+        {
+            MainViewModel.GetInstance().Land = new LandViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+        }
+        #endregion
+    }
+}
---
+++ Lands/ViewModels/LandViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Lands.Models;
+
+namespace Lands.ViewModels
+{
+    public class LandViewModel
+    {
+        #region Properties
+        public Land Land { get; set; }
+        #endregion
+
+        #region Constructors
+        public LandViewModel()
+        {
+        }
+
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        }
+        #endregion
+    }
+}
